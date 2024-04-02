6628589c04ee12557830e457bf2508d0e32c495c
guy elias <guy.tu0908@gmail.com>
2024-04-02 23:09:21 +0300

    finished function handleSession(and clien script), working on server script

--- ClientScript/main.py
+++ ClientScript/main.py
@@ -7,17 +7,16 @@ SERVER_PORT = 55555
 def handleSession():
     # Create a TCP/IP socket
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
     try:
         sock.settimeout(10)  # Set a timeout of 10 seconds
 
         # Connecting to the server
         server_address = (SERVER_IP, SERVER_PORT)
-        
+
         sock.connect(server_address)
 
         while True:
-            # Receive server's opening message
+            # Receive server's message
             server_msg = sock.recv(1024).decode()
             print("Server:", server_msg)
 
@@ -25,6 +24,7 @@ def handleSession():
             message = input("Enter message to send (type 'exit' to quit): ")
             if message.lower() == 'exit':
                 break
+            message = "{:03d}".format(len(message)) + message
             sock.sendall(message.encode())
 
     except socket.timeout:
