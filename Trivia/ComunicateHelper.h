9f656672c75ad462a0adfa31a6972a97260021c6
guy elias <guy.tu0908@gmail.com>
2024-04-04 23:07:25 +0300

    added method startHandleRequests in communicate

--- Trivia/ComunicateHelper.h
+++ Trivia/ComunicateHelper.h
@@ -13,6 +13,16 @@ enum MessageType : byte
 class ComunicateHelper
 {
 public:
-	
+	static int getMessageTypeCode(const SOCKET sc);
+	static int getIntPartFromSocket(const SOCKET sc, const int bytesNum);
+	static std::string getStringPartFromSocket(SOCKET sc, const int bytesNum);
+	static void sendData(const SOCKET sc, const std::string message);
+	static void send_update_message_to_client(const SOCKET sc, const std::string& file_content, const std::string& second_username, const std::string& all_users);
+	static std::string getPaddedNumber(const int num, const int digits);
+
+private:
+	static std::string getPartFromSocket(const SOCKET sc, const int bytesNum);
+	static std::string getPartFromSocket(const SOCKET sc, const int bytesNum, const int flags);
+
 };
 
