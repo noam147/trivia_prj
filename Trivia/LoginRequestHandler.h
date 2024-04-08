5fe532533d2a862db908d140bb3731100bba8393
noam <noamar1234567@gmail.com>
2024-04-08 13:54:22 +0000

    Replace LoginRequestHandler.h

--- Trivia/LoginRequestHandler.h
+++ Trivia/LoginRequestHandler.h
@@ -3,7 +3,8 @@
 
 class LoginRequestHandler : public IRequestHandler
 {
-private:
-
+public:
+	bool isRequestRelevant(RequestInfo info) override;//func check if msg is sign in or login type of msg
+	RequestResult handleRequest(RequestInfo info) override;//need to fix
 };
 
