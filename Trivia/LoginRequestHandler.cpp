f4618356d7cf5b3f798a98c0546f674e136be164
noam <noamar1234567@gmail.com>
2024-04-08 13:54:35 +0000

    Replace LoginRequestHandler.cpp

--- Trivia/LoginRequestHandler.cpp
+++ Trivia/LoginRequestHandler.cpp
@@ -1,1 +1,25 @@
 #include "LoginRequestHandler.h"
+
+bool LoginRequestHandler::isRequestRelevant(RequestInfo info)
+{
+	if (info.buffer[0] == LOGIN_REQUEST || info.buffer[0] == SIGN_UP_REQUEST)
+	{
+		return true;
+	}
+	return false;
+}
+
+RequestResult LoginRequestHandler::handleRequest(RequestInfo info)
+{
+	RequestResult r;
+	
+	if (isRequestRelevant(info))
+	{
+		if (info.buffer[0] == LOGIN_REQUEST)
+		{
+			//r.response = JsonRequestPacketDeserializer::deserializeLoginRequest(info.buffer);
+		}
+		
+	}
+	return RequestResult();
+}
