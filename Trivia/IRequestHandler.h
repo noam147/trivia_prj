83eeb8fb63010246aeea56f3e23282ad7a6d5138
noam <noamar1234567@gmail.com>
2024-04-08 13:55:04 +0000

    Replace IRequestHandler.h

--- Trivia/IRequestHandler.h
+++ Trivia/IRequestHandler.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <ctime>
+#include <vector>
+#include "CommunicationHelper.h"
+#include "JsonRequestPacketDeserializer.h"
+struct RequestResult;//declare struct here
+struct RequestInfo {
+	int id;//request id
+	std::time_t receivalTime;
+	std::vector<char> buffer;
+
+};
 class IRequestHandler
 {
+	virtual bool isRequestRelevant(RequestInfo info) =0;
+	virtual RequestResult handleRequest(RequestInfo info) =0;
 };
 
+struct RequestResult//actualize struct here
+{
+	char* response;
+	IRequestHandler* newHandler;
+};
